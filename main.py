9f3a272e6aabf642d14475a8ccde6029637c3c3e
Lautaro Matas <lmatas@gmail.com>
2024-04-02 18:47:07 +0200

    update

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 
-import re
 import sys
 from textwrap import indent
 from unittest import result
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import HTTPException
 
-import awswrangler as wr
 from config import read_ini
 from opensearchpy import OpenSearch, exceptions
 
-
+from lareferenciastatsdb import UsageStatsDatabaseHelper, IdentifierPrefixNotFoundException
 
 app = FastAPI(
     title="LA Referencia Usage Statistics API",
@@ -45,9 +44,25 @@ try:
     # read cors file
     with open(cors_filename, "r") as f:
         origins = f.read().splitlines()
+
+    ## dbhelper
+    dbhelper = UsageStatsDatabaseHelper(config)    
+
+    # opensearch
+    host = config["OPENSEARCH"]["HOST"]
+    port = int(config["OPENSEARCH"]["PORT"])
+    is_ssl = str(config["OPENSEARCH"]["SSL"]).lower() == "true"
+    auth = (config["OPENSEARCH"]["USER"], config["OPENSEARCH"]["PASSWORD"]) 
+
+    client = OpenSearch(
+        hosts = [{'host': host, 'port': port}],
+        http_compress = True, # enables gzip compression for request bodies
+        use_ssl = is_ssl,
+    )
         
+
 except Exception as e:
-    print("Error reading config file: %s" % e)
+    print("Error: %s" % e)
     sys.exit(1)
 
 
@@ -60,27 +75,11 @@ app.add_middleware(
 )
 
 
-def build_index_name(source, country = '*', year = '*'):
-    source = source.replace("::", "_")
-    index_name = 'usage_stats_%s_%s_%s' % (country, source, year)
-    index_name = index_name.lower() 
-    return index_name
 
 
-@app.get("/report/repositoryWidget")
-async def repositoryWidget(identifier: str = None, source: str = '*', start_date: 'str' = 'now-1y', end_date: 'str' = 'now', time_unit : str = 'year'):
+def parametrize_query(identifier, start_date, end_date, time_unit):
 
-    host = config["OPENSEARCH"]["HOST"]
-    port = int(config["OPENSEARCH"]["PORT"])
-    auth = (config["OPENSEARCH"]["USER"], config["OPENSEARCH"]["PASSWORD"]) 
-
-    client = OpenSearch(
-        hosts = [{'host': host, 'port': port}],
-        http_compress = True, # enables gzip compression for request bodies
-        use_ssl = False
-    )
-
-    query = {
+    query = { 
        "aggs": {
             "views": { "sum": { "field": "views" }},
             "downloads": { "sum": { "field": "downloads" }},
@@ -152,31 +151,117 @@ async def repositoryWidget(identifier: str = None, source: str = '*', start_date
         "track_total_hits": "false"
     }
 
-    # if identifier is None then remove the identifier filter
-    # if identifier is None:
-    #     del query['query']['bool']['should'][0]
-    #     del query['query']['bool']['minimum_should_match']
+    # if identifier is None, then remove the identifier from the query
+    if identifier is None:
+        del query["query"]["bool"]["should"]
+        del query['query']['bool']['minimum_should_match']
 
-    # indices = set()
+    return query
+  
 
-    # indices.add(build_index_name(regional_source, '00', '*'))
-    # indices.add(build_index_name(national_source, country, '*'))
-    # indices.add(build_index_name(repository_source, country, '*'))
+## repositoryWidget endpoint
+@app.get("/report/itemWidget")
+async def itemWidget(identifier: str = None, source: str = '*', start_date: 'str' = 'now-1y', end_date: 'str' = 'now', time_unit : str = 'year'):
+
+    # parametrize the query based on the parameters
+    query = parametrize_query(identifier, start_date, end_date, time_unit)
     
-    # indices = list(indices)
-
-
-    indices = ["test-processor-*"]
-
     try:
+
+        try: 
+            ## first try to get the indices from the identifier (this works if the repository is registered in the database)
+            indices = dbhelper.get_indices_from_identifier(identifier)
+        except IdentifierPrefixNotFoundException as e:
+            ## if the identifier is not found in the database, then try to get the indices from the source (this will get national and regional statistics only)
+            indices = dbhelper.get_indices_from_source(source)
+
+        if len(indices) == 0:
+            raise HTTPException(status_code=404, detail="The source %s and identifier %s are not present in the database" % (source, identifier))
+        
         response = client.search(
             body = query,
             index = ','.join(indices),
         )
-    except exceptions.NotFoundError:
-        print("Index not found: %s" % ','.join(indices))
-        return {}
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
 
+    if response is None or response.get("aggregations") is None:
+        raise HTTPException(status_code=404, detail="Not found")   
+ 
     return response.get("aggregations", {})
 
 
+
+## repositoryWidget endpoint
+@app.get("/report/repositoryWidget")
+async def repositoryWidget(source_id: str = '*', start_date: 'str' = 'now-1y', end_date: 'str' = 'now', time_unit : str = 'year'):
+
+    source = dbhelper.get_source_by_id(source_id)
+
+    if source is None:
+        raise HTTPException(status_code=404, detail="The source %s is not present in the database" % (source_id))
+    
+    if source.type != "R":
+        raise HTTPException(status_code=404, detail="The source %s is not a repository" % (source))
+
+    #try:
+
+    identifier_prefix = dbhelper.get_identifier_prefix_from_source(source_id)
+    print("identifier_prefix: %s" % identifier_prefix)
+
+    identifier_pattern = identifier_prefix + "*"
+    query = parametrize_query(identifier_pattern, start_date, end_date, time_unit)
+
+    indices = dbhelper.get_indices_from_identifier(identifier_prefix)
+
+    print("indices: %s" % indices)
+
+    if len(indices) == 0:
+        raise HTTPException(status_code=404, detail="The source %s is not present in the database" % (source))
+    
+    response = client.search(
+        body = query,
+        index = ','.join(indices),
+    )
+    #except Exception as e:
+    #    raise HTTPException(status_code=404, detail=str(e))
+
+    if response is None or response.get("aggregations") is None:
+        raise HTTPException(status_code=404, detail="Not found")   
+ 
+    return response.get("aggregations", {})
+
+
+## repositoryWidget endpoint
+@app.get("/report/nationalWidget")
+async def repositoryWidget(source_id: str = '*', start_date: 'str' = 'now-1y', end_date: 'str' = 'now', time_unit : str = 'year'):
+
+    source = dbhelper.get_source_by_id(source_id)
+
+    if source is None:
+        raise HTTPException(status_code=404, detail="The source %s is not present in the database" % (source_id))
+    
+    if source.type != "N":
+        raise HTTPException(status_code=404, detail="The source %s is not a national aggregator" % (source))
+
+
+    indices = dbhelper.get_indices_from_national(source_id)
+
+    print("indices: %s" % indices)
+
+    if len(indices) == 0:
+        raise HTTPException(status_code=404, detail="The source %s is not present in the database" % (source))
+    
+    query = parametrize_query(None, start_date, end_date, time_unit)
+
+    response = client.search(
+        body = query,
+        index = ','.join(indices),
+    )
+    #except Exception as e:
+    #    raise HTTPException(status_code=404, detail=str(e))
+
+    if response is None or response.get("aggregations") is None:
+        raise HTTPException(status_code=404, detail="Not found")   
+ 
+    return response.get("aggregations", {})
