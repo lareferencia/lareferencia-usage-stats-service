bd10610bf83eac92cfdba0c7837b69f3c6aa5fe9
Lautaro Matas <lmatas@gmail.com>
2024-09-18 10:24:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -293,7 +293,7 @@ async def itemWidget(identifier: str = None, source: str = '*', start_date: 'str
     query = parametrize_query(identifier, start_date, end_date, time_unit)
 
     # get the source
-    if source_id != '*':
+    if source_id is not None and source_id != "" and source_id != '*':
         source = dbhelper.get_source_by_id(source_id)
         if source is None:
             raise HTTPException(status_code=404, detail="The source %s is not present in the database" % (source_id))
@@ -307,6 +307,7 @@ async def itemWidget(identifier: str = None, source: str = '*', start_date: 'str
         except IdentifierPrefixNotFoundException as e:
             ## if the identifier is not found in the database, then try to get the indices from the source (this will get national and regional statistics only)
             indices = dbhelper.get_indices_from_source(index_prefix, source)
+            print ("identifier not found: %s", identifier)
 
         if len(indices) == 0:
             raise HTTPException(status_code=404, detail="The source %s and identifier %s are not present in the database" % (source_id, identifier))
